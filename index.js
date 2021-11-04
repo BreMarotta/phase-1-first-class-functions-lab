881cd69b3437380e96c0964f987af858630f93f9
Breanne <BreMarotta@gmail.com>
2021-11-04 12:53:45 -0400

    commit tree 9232a4eb9ab36054acbdf5b9626d6f2c35a66fb8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers= (array)=>{
+    const a= [...array.slice(0,2)];
+    return a;
+}
+
+const returnLastTwoDrivers= (array)=>{
+    const b= [...array.slice(array.length-2, array.length)];
+    return b;
+}
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(cb){
+    return(x) =>{
+        return (x*x);
+    };
+}
+
+const fareDoubler=x=>{
+    return x*2};
+
+const fareTripler=x=>{
+    return x*3};
+
+function selectDifferentDrivers(array, result){
+     //array;
+    return result(array);
+}
